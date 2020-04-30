94c691791d1223d27d326cd4b327c6fb032e0fc8
hask777 <haskellisp@gmail.com>
2020-04-30 10:34:33 +0400

    movie card evo

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -1,9 +1,9 @@
-<div class="movie_item relative flex">
+<div class="movie_item relative">
     <a href="{{ route('movies.show', $movie['id']) }}">
         <img src="{{ 'https://image.tmdb.org/t/p/w500/' . $movie['poster_path'] }}" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-150">
-        <div class="bg-black opacity-75 w-100 absolute bottom-0 left-0">
+        <div class="movie_item_overlay bg-black  w-100">
             <span class="p-2 block text-orange-500">{{$movie['title']}}</span>
-            <span>{{$movie['release_date']}}</span>
+            <span class="p-2 block text-orange-500">{{$movie['release_date']}}</span>
         </div>
     </a>
 </div>
