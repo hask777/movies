507ef6a1abc98530b897dc80c40afbcf817802dd
hask777 <haskellisp@gmail.com>
2020-05-01 13:50:28 +0400

    Add cool face API

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="{{secure_asset('css/main.css')}}">
-    <link rel="stylesheet" href="{{secure_asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/main.css')}}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <livewire:styles>
     </head>
