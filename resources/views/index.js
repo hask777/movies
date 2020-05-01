a7c151ccd0253526d80aff6b8c42170c2d721fce
hask777 <haskellisp@gmail.com>
2020-05-01 13:42:54 +0400

    Add cool face API

--- resources/views/index.js
+++ resources/views/index.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'resources/views'))
+  .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
+  .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
