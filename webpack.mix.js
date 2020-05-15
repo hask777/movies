ae779ed343b84b15e33eedc42725736372eedcf4
hask777 <haskellisp@gmail.com>
2020-05-15 18:51:30 +0400

    'dd'

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/main.css', 'public/css', [
-        require('tailwindcss'),
+        require('tailwindcss'), 
     ]);
 
 mix.sass('resources/sass/app.scss', 'public/css');
