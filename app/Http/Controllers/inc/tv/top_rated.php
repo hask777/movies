1d77a6b33b1d6f75a1a861646caaf23303e5bd1a
hask777 <haskellisp@gmail.com>
2020-05-17 00:28:31 +0400

    'ff'

--- app/Http/Controllers/inc/tv/top_rated.php
+++ app/Http/Controllers/inc/tv/top_rated.php
@@ -4,4 +4,4 @@ $topRatedTv = Http::withToken(config('services.tmdb.token'))
     ->get('https://api.themoviedb.org/3/tv/top_rated?page=1&language=ru-RU')
     ->json()['results'];
 
-dump($topRatedTv);
+// dump($topRatedTv);
